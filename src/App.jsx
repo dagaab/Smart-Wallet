182dfd885fd9a8535762bef639de23c1cff51b64
Dagaab <daga.abramowicz@gmail.com>
2024-03-17 17:14:33 +0000

    added a footer

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import './App.css'
 import Nav from "./components/Navbar";
 import { Grommet } from 'grommet';
 import Grid from "./components/Grid";
+import Footer from "./components/Footer";
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
       <Nav />
       <Pet />
       <Grid />
+      <Footer />
     </Grommet>
   );
 }
---
+++ src/components/Footer.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Footer () {
+    return (
+        <footer>
+          <p>
+           Made by group 14 &#x1F436;
+          </p>
+        </footer>
+      );
+}
+export default Footer;
