12e5334aea8777a6dbbfd55c0f07464bdceb961f
RajpreetKR <rajpreet@khalsa.com>
2024-03-19 19:04:48 +0000

    user can now show/hide their recent purchases

--- src/components/Recent.jsx
+++ src/components/Recent.jsx
@@ -1,19 +1,57 @@
-import React from "react";
-import "../styles/Recent.css";
-import Categories from './Categories'; 
+import React, { useState, useEffect } from 'react';
+import { Grommet, Box, Button, Meter } from 'grommet';
 
-console.log(Categories);
+const Recent = () => {
+  const [view, setView] = useState('select');
+  const [spending, setSpending] = useState({});
+  const [totalSpending, setTotalSpending] = useState(0);
 
-// By importing the Section.css file, it is added to the DOM whenever this component loads
-function Recent() {
+  useEffect(() => {
+    const spendingData = JSON.parse(localStorage.getItem('spending'));
+    setSpending(spendingData);
+    const totalSpendingData = JSON.parse(localStorage.getItem('totalSpending'));
+    setTotalSpending(totalSpendingData);
+  }, []);
+
+  // Calculate the percentages for the meter
+  const meterValues = Object.entries(spending).map(([category, amount]) => ({
+    label: category,
+    value: (amount / totalSpending) * 100,
+  }));
+
   return (
-    <section className="recent-section">
-      <h2>Recent Purchases</h2>
-      <p>
-        Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque
-      </p>
-    </section>
+    <Grommet>
+      <Box>
+        <Button onClick={() => setView('selected')}>Show Recent Spending</Button>
+      </Box>
+      {view === 'selected' && (
+        <Box>
+          <Button onClick={() => setView('select')}>Hide Recent Spending</Button>
+          {spending && (
+      <Box direction="column" border={{ color: 'black', size: 'medium' }} pad="small">
+        {Object.entries(spending).map(([category, amount], index) => (
+        <Box key={index}>
+              {category}: {amount}
+        </Box>
+      ))}
+  </Box>
+)}
+
+          <Box direction="row" border={{ color: 'black', size: 'medium' }}>
+            Total Spending: {totalSpending}
+          </Box>
+          <Box align="center" pad="large">
+            <Meter
+              type="pie"
+              background="light-2"
+              size="small"
+              values={meterValues}
+            />
+          </Box>
+        </Box>
+      )}
+    </Grommet>
   );
-}
+};
 
 export default Recent;
